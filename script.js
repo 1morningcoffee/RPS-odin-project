d406b83d9cc4adea234082baa35f99418c07a2bb
1morningcoffee <110513082+1morningcoffee@users.noreply.github.com>
2024-06-12 16:00:55 -0400

    Add function to play each round

--- script.js
+++ script.js
@@ -20,3 +20,33 @@ function getHumanChoice() {
     return humanInput;
 }
 
+// function to compare inputs and play the round
+function playRound() {
+    let pInput = getHumanChoice();
+    let computerInput = getComputerChoice();
+    if (pInput !== "rock" && pInput !== "paper" && pInput !== "scissors") {
+        playRound();
+    } else {
+        if (pInput === computerInput) {
+            console.log("It is a tie round!");
+        } else if (pInput === "rock" && computerInput === "paper") {
+            console.log("Paper covers rock. The computer wins this round!");
+            computerScore++;
+        } else if (pInput === "rock" && computerInput === "scissors") {
+            console.log("Rock smashes scisors. The player wins this round!");
+            playerScore++;
+        } else if (pInput === "paper" && computerInput === "rock") {
+            console.log("Paper covers rock. The player wins this round!");
+            playerScore++;
+        } else if (pInput === "paper" && computerInput === "scissors") {
+            console.log("Scissors cut paper. The computer wins this round!");
+            computerScore++;
+        } else if (pInput === "scissors" && computerInput === "rock") {
+            console.log("Rock smashes scissors. The computer wins this round!");
+            computerScore++;
+        } else if (pInput === "scissors" && computerInput === "paper") {
+            console.log("Scissors cut paper. The player wins this round!");
+            playerScore++;
+        }
+    }
+}
